d0ae5905010414ef8e4753d81ca37710dcc41a14
Sebastian Gomez <seagomezar@gmail.com>
2015-11-02 14:51:42 -0300

    Adding colors, givin structure to project

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,93 @@ var gulp = require('gulp'),
 	eslint = require('gulp-eslint')
 	browserSync = require('browser-sync').create();
 
+gulp.task('main', function()
+{
+  gulp.src(['app/index.html'])
+	.pipe(usemin({
+			css: [minifyCSS(), 'concat'],
+			js: [ngAnnotate(), 'concat']
+	}))
+	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('views', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+
+  return gulp.src(['app/views/*.html','app/views/**/*.html','app/views/**/**/*.html'])
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('imagenes', function () {
+    return gulp.src(['app/images/*.*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('extras', function () {
+    return gulp.src(['app/favicon.ico','app/robots.txt'])
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['dist/**/*.*','dist/**/**/*.*','dist/*.*','dist/*'], { read: false })
+           .pipe(clean({ force: true }));
+});
+gulp.task('build', ['main','views','imagenes','fonts','extras','extras1']);
+
+gulp.task('success', ['clean'], function() {
+  gulp.start('build');
+});
+
+gulp.task('default',['lint'],function(){
+	gulp.start('success');
+});
+
 gulp.task('serve', function (){
   browserSync.init({
     notify: false,
     port: 9000,
-		server:{
-			baseDir: ['']
-		}
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
   });
 
   gulp.watch([
-    '*.html',
-    '*.js',
-    '*.css'
+    'app/*.html',
+    'app/views/*.html',
+    'app/views/**/*.html',
+    'app/scripts/*.js',
+    'app/scripts/**/*.js',
+    'app/scripts/**/**/*.js',
+    'bower_components/*',
   ]).on('change', browserSync.reload);
+
+  gulp.watch('app/styles/**/*.css', ['styles']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json');
+});
+
+gulp.task('serve:dist', function (){
+  gulp.start('default');
+  browserSync.init({
+    notify: false,
+    port: 3000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['app/scripts/**/*.js','app/scripts/**/**/*.js','app/scripts/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
